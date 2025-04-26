011101d0c4fed88f233db6a6650ad1cef52e4b89
Your name <your-email@example.com>
2025-04-27 03:19:53 +0530

    some changes

--- video 80 clASSes and object/script.js
+++ video 80 clASSes and object/script.js
@@ -1,1 +1,35 @@
-console.log("you are not setup")
+// let obj={
+//     a:1,
+//     b:"harry"
+// }
+// console.log(obj)
+
+class Animal {
+    constructor(name) {
+        this.name=name;
+        console.log("The object is created")
+    }
+    jump(){
+        console.log("kood rha hoon")
+    }
+    eat(){
+        console.log("kha rha hoon")
+    }
+}
+class Lion extends Animal {
+    constructor(name) {
+        super(name);
+        console.log("The object is created and he is a lion ....")
+    }
+    eat(){
+        console.log("kha rha hoon insaaono ko")
+    }
+    
+
+}
+
+let a=new Animal("shera");
+console.log(a)
+ 
+let l=new Lion("champak");
+console.log(l)
